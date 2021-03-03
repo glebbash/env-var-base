cf8e56beec68ccc6561ca556a6017d22b59f17ec
glebbash <glebbash@gmail.com>
2021-03-03 11:25:43 +0200

    feat: implement BaseConfig

--- src/env-var-base.ts
+++ src/env-var-base.ts
@@ -1,6 +1,40 @@
-// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-  // import "core-js/fn/array.find"
-  // ...
-export default class DummyClass {
+import { from } from 'env-var'
+import { config } from 'dotenv'
 
+/**
+ * Base configuration class that uses env-var and dotenv
+ *
+ * Usage:
+ * ```
+ * class AppConfig extends BaseConfig {
+ *  port = this.get('PORT').default(3000).asPortNumber()
+ * }
+ *
+ * const config = new AppConfig()
+ * console.log(config.port) // 3000
+ * ```
+ */
+export class BaseConfig {
+  protected env: ReturnType<typeof from>
+
+  constructor(env?: Record<string, string>) {
+    if (env) {
+      this.env = from(env)
+    } else {
+      config()
+      this.env = from(process.env)
+    }
+  }
+
+  /**
+   * Usage:
+   * ```
+   * port = this.get('PORT').default(3000).asPortNumber()
+   * ```
+   * @param name
+   */
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  protected get(name: string) {
+    return this.env.get(name)
+  }
 }
--- test/env-var-base.test.ts
+++ test/env-var-base.test.ts
@@ -1,14 +1,36 @@
-import DummyClass from "../src/env-var-base"
+import { BaseConfig } from '../src/env-var-base'
+import * as envVar from 'env-var'
+import * as dotenv from 'dotenv'
 
-/**
- * Dummy test
- */
-describe("Dummy test", () => {
-  it("works if true is truthy", () => {
-    expect(true).toBeTruthy()
+describe('env-var-base', () => {
+  afterEach(() => jest.restoreAllMocks())
+
+  it('uses provided env obj', () => {
+    const fromSpy = jest.spyOn(envVar, 'from')
+    const env = {}
+
+    new BaseConfig(env)
+
+    expect(fromSpy).toBeCalledWith(env)
   })
 
-  it("DummyClass is instantiable", () => {
-    expect(new DummyClass()).toBeInstanceOf(DummyClass)
+  it('defaults to process.env and calls dotenv', () => {
+    const fromSpy = jest.spyOn(envVar, 'from').mockReturnThis()
+    const dotenvConfigSpy = jest.spyOn(dotenv, 'config').mockReturnThis()
+
+    new BaseConfig()
+
+    expect(dotenvConfigSpy).toBeCalled()
+    expect(fromSpy).toBeCalledWith(process.env)
+  })
+
+  it('works when extending class', () => {
+    class AppConfig extends BaseConfig {
+      port = this.get('PORT').required().asPortNumber()
+    }
+
+    const config = new AppConfig({ PORT: '3000' })
+
+    expect(config.port).toBe(3000)
   })
 })
